19a8a2ecbef733142e9418ece91b4661fed1ea60
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-03-30 03:15:31 +0300

    feat: setup release

---
+++ .releaserc.js
@@ -0,0 +1,33 @@
+const conventionalChangelogOptions = {
+  preset: 'conventionalcommits',
+  // https://github.com/conventional-changelog/conventional-changelog-config-spec/blob/master/versions/2.1.0/README.md
+  presetConfig: {},
+}
+
+module.exports = {
+  branches: [
+    'master',
+  ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      // https://github.com/semantic-release/commit-analyzer#options
+      {
+        ...conventionalChangelogOptions,
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      // https://github.com/semantic-release/release-notes-generator#options
+      {
+        ...conventionalChangelogOptions,
+      },
+    ],
+    // https://github.com/semantic-release/changelog#options
+    '@semantic-release/changelog',
+    // https://github.com/semantic-release/npm
+    '@semantic-release/npm',
+    // https://github.com/semantic-release/git#options
+    '@semantic-release/git',
+  ],
+}
